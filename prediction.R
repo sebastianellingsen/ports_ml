776aba76fa3ea755e861fa53cc23fafdfd328686
sebastianellingsen <sebastian.ellingsen@upf.edu>
2018-12-08 21:27:53 -0600

    I added ROC curves

--- prediction.R
+++ prediction.R
@@ -9,15 +9,28 @@
 ## Loading data and packages ##
 ###############################
 
-source("data_prep.R")
+#source("data_prep.R")
 
 if (!require(pacman)) install.packages("pacman")
-p_load(ranger, Metrics, dplyr, purr, broom, rsample)
-
+p_load(ranger, Metrics, broom, rsample, tidyverse)
 
 ######################
 ## Model evaluation ##
 ######################
+library(tictoc)
+tic()
+generating_data()
+toc()
+save(dataset, file="dataset.Rda")
+load("dataset.Rda")
+
+
+# Generate final dataset used to predict
+data <- dataset[which(dataset$X1!="inland"),]
+data$sample <- rbinom(n=nrow(data),prob=.15,size=1)
+data <- data %>% filter(y == 1 | (sample == 1 & y!=1))
+
+
 
 ## Splitting the data to training, testing  
 data_split <- initial_split(data, 0.75)
@@ -25,23 +38,22 @@ training_data <- training(data_split)
 testing_data <- testing(data_split)
 
 ## Cross validation folds
-cv_split <- vfold_cv(training_data, v = 5)
+cv_split <- vfold_cv(training_data, v = 10)
 cv_data <- cv_split %>% 
   mutate(train = map(splits, ~training(.x)), validate = map(splits, ~testing(.x)))
 
 ## Fitting the model
 cv_models <- cv_data %>% 
-  mutate(forest_model = map(train, ~ranger(formula= port~., data=data, num.trees = 100, mtry = 10)))
+  mutate(forest_model = map(train, ~ranger(formula= y~., data=.x, num.trees = 100, mtry = 10)))
 
 cv_prep <- cv_models %>% 
-  mutate(validate_actual = map(validate, ~.x$port)) %>% 
+  mutate(validate_actual = map(validate, ~.x$y)) %>% 
   mutate(predicted = map2(forest_model, validate, ~predict(.x,.y)$predictions)) 
 
 ## Validating the fit of the model
-cv_eval <- cv_prep %>% mutate(y_pred = map(cv_eval$predicted, ~ifelse(.x>0.5, 1, 0))) 
+cv_eval <- cv_prep %>% mutate(y_pred = map(cv_prep$predicted, ~ifelse(.x>0.3, 1, 0))) 
 
 cv_eval <-  cv_eval %>% 
-  mutate(y_pred = map(cv_eval$predicted, ~ifelse(.x>0.5, 1, 0))) %>% 
   mutate(error = map2_dbl(validate_actual, y_pred, ~mae(.x,.y))) %>% 
   mutate(precision = map2_dbl(validate_actual, y_pred, ~precision(.x,.y))) %>% 
   mutate(accuracy = map2_dbl(validate_actual, y_pred, ~accuracy(.x,.y)))
@@ -51,23 +63,111 @@ confusion_matrix <- map2(cv_eval$validate_actual, cv_eval$y_pred, ~table(.x,.y))
 er <- map(cv_eval$error, ~mean(.x)) %>% unlist() %>% sort(decreasing=TRUE)
 plot(er)
 
-## Fitting the full model to the training data
-model <- ranger(formula= port~., data=training_data, num.trees = 100, mtry = 10)
+# ROC curves
+library(ROCR)
 
-prediction <- predict(model, data)$predictions
-y_pred <- ifelse(predict(fmodel, df)$predictions>0.3, 1, 0)
-eval_df <- data.frame(y, y_pred, prediction)
+data(ROCR.simple)
+pred <- prediction( ROCR.simple$predictions, ROCR.simple$labels)
+perf <- performance(pred,"tpr","fpr")
+plot(perf)
+
+pred <- prediction(cv_eval$predicted[1], cv_eval$y_pred[1])
+perf <- performance(pred,"tpr","fpr")
+
+
+
+
+# Make many roc curves for each model
+
+
+
+
+
+## Fitting the full model to the training data
+model <- ranger(formula= y~., data=training_data, num.trees = 1000, mtry = 300)
+
+prediction <- predict(model, testing_data)$predictions
+
+pred <- prediction(prediction, testing_data$y)
+perf <- performance(pred, "tpr", "fpr")
+
+plot(perf)
+
+
+x_rf <- perf@alpha.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf <- perf@y.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf <- y_rf[x_rf<=1]
+x_rf <- x_rf[x_rf<=1]
+
+
+
+model <- ranger(formula= y~., data=training_data, num.trees = 10, mtry = 300)
+
+prediction <- predict(model, testing_data)$predictions
+
+pred <- prediction(prediction, testing_data$y)
+perf <- performance(pred, "tpr", "fpr")
+
+x_rf1 <- perf@alpha.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf1 <- perf@y.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf1 <- y_rf1[x_rf1<=1]
+x_rf1 <- x_rf1[x_rf1<=1]
+
+
+
+model <- ranger(formula= y~., data=training_data, num.trees = 30, mtry = 150)
+
+prediction <- predict(model, testing_data)$predictions
+
+pred <- prediction(prediction, testing_data$y)
+perf <- performance(pred, "tpr", "fpr")
+
+x_rf2 <- perf@alpha.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf2 <- perf@y.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf2 <- y_rf2[x_rf2<=1]
+x_rf2 <- x_rf2[x_rf2<=1]
+
+
+model <- ranger(formula= y~., data=training_data, num.trees = 50, mtry = 80)
+
+prediction <- predict(model, testing_data)$predictions
+pred <- prediction(prediction, testing_data$y)
+perf <- performance(pred, "tpr", "fpr")
+x_rf3 <- perf@alpha.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf3 <- perf@y.values %>% unlist() %>% sort(decreasing=TRUE) 
+y_rf3 <- y_rf3[x_rf3<=1]
+x_rf3 <- x_rf3[x_rf3<=1]
+
+
+
+ggplot() + 
+  geom_line(aes(x_rf, y_rf), color="Blue") + 
+  #geom_line(aes(x_rf1, y_rf1), color="Red") +
+  geom_line(aes(x_rf2, y_rf2), color="Green") +
+  geom_line(aes(x_rf3, y_rf3), color="Orange") +
+  xlab("False positive rate") + ylab("True positive rate") + ggtitle("Receiver operating characteristic") +
+  geom_segment(aes(x = 0, y = 0, xend = 0.98, yend = 1, colour = "segment"),alpha=0.3, linetype = 2) +
+  scale_x_continuous(limits = c(0, 1)) + theme_classic() 
+                     
+
+
+
+# shows the roc of a pruned random forest
+
+
+
 
 
 ###########################
 ## Joining the dataframe ##
 ###########################
 
-hexagons <- gIntersection(hexagons, iceland, byid = TRUE)
-ID <- sapply(hexagons@polygons, function(x) x@ID)
+hexagons_full <- gIntersection(hexagons, iceland, byid = TRUE)
+
+ID <- sapply(hexagons_full@polygons, function(x) x@ID)
 row.names(eval_df) <- ID
 
-sps_df <- SpatialPolygonsDataFrame(hexagons, eval_df, match.ID = TRUE)
+sps_df <- SpatialPolygonsDataFrame(hexagons_full, eval_df, match.ID = TRUE)
 
 
 #######################
@@ -76,10 +176,22 @@ sps_df <- SpatialPolygonsDataFrame(hexagons, eval_df, match.ID = TRUE)
 
 tm_shape(sps_df) +
   tm_fill(col="prediction", palette=plasma(256)) + tm_layout(frame=FALSE) +
-  tm_shape(hexagons) +
-  tm_borders(col = "white") +
-  tm_shape(ports_iceland) +
-  tm_dots(size=0.1) 
+  tm_shape(hexagons_full) + tm_borders(col = "white") +
+  tm_shape(ports_iceland) + tm_dots(size=0.1) 
+
+
+
+
+
+
+# try removing areas that are not along the coast, sample size low since I'm doing
+# cross validation. Gjor dette i datadelen, ved a tydelig markere de som ikke skal 
+# vaere der.
+# make a receiver operating characeristic for each model.
+
+
+
+
 
 
 
