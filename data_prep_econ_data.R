8810538ad32e095c9a4a5d6144b9470421ba2f94
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-01-24 21:16:21 -0600

    added data about landlocked countries

--- data_prep_econ_data.R
+++ data_prep_econ_data.R
@@ -1,5 +1,9 @@
 
 
+##############################################
+## Generating  dataset at the country level ##
+##############################################
+
 if (!require(pacman)) install.packages("pacman")
 p_load(tidyverse, sf, raster, tmap, sp, rgdal, rgeos, viridis, ranger, tmaptools)
 
@@ -7,13 +11,6 @@ p_load(tidyverse, sf, raster, tmap, sp, rgdal, rgeos, viridis, ranger, tmaptools
 theme_set(theme_bw() + theme(panel.grid.minor = element_line(colour = "white", size = 0.5),
                              panel.grid.major = element_line(colour = "white", size = 0.2)))
 
-# should randomly reorder the data as well?
-
-
-
-###########################
-## Joining the dataframe ##
-###########################
 #hexagons_full <- gIntersection(hexagons, study_area, byid = TRUE)
 
 model <- ranger(formula= as.numeric(y)~., data=training_data, num.trees = 4000, mtry = 5)
@@ -24,36 +21,6 @@ row.names(dataset_final) <- row.names(data)
 
 sps_df <- SpatialPolygonsDataFrame(hexagons, dataset_final, match.ID = TRUE)
 
-
-#######################
-## Plotting the data ##
-#######################
-
-# Plotting a country
-plot_country <- function(x){
-  
-  # Define the datasets
-  study_area_country <- countries10[countries10$ADMIN==x,]
-  dataset_country <- gIntersection(sps_df, study_area_country, byid = TRUE)
-  ports_country <<- gIntersection(study_area_country, ports, byid = TRUE)
-  
-  # Set the same row names and match the datasets
-  row.names(dataset_country) <- gsub("\\s.*", "", sapply(dataset_country@polygons, function(x) x@ID))
-  dataset_country_final <<- sps_df[row.names(sps_df) %in% sapply(dataset_country@polygons, function(x) x@ID), ]
-}
-
-plot_country("Spain")
-
-# Defining variables
-dataset_country_final@data$pred <- ifelse(dataset_country_final@data$y_pred>=1.5, 1, 0)
-dataset_country_final@data$false_p <- ifelse(as.numeric(dataset_country_final@data$y)-1<dataset_country_final@data$pred, 2, 0)
-dataset_country_final@data$false_n <- ifelse(as.numeric(dataset_country_final@data$y)-1>dataset_country_final@data$pred, 1, 0)
-dataset_country_final@data$errors <- dataset_country_final@data$false_p + dataset_country_final@data$false_n
-
-# Plotting the predicted ports
-p1 <- tm_shape(dataset_country_final) +  tm_fill(col="y_pred", palette=plasma(256)) + tm_layout(frame=FALSE, legend.show=FALSE)
-p2 <- tm_shape(dataset_country_final) +  tm_fill(col="errors", palette=plasma(256)) + tm_layout(frame=FALSE, legend.show=FALSE)
-tmap_arrange(p1,p2)
 
 
 ## Generating country level dataset
@@ -95,8 +62,9 @@ excel_sheets("data/Trade_of_Goods.xlsx")
 trade_data <- read_excel("data/Trade_of_Goods.xlsx", skip=5) %>% 
   select(-"Base Year", -"Scale") %>% 
   replace_with_na_all(condition=~.x=="...") %>% 
+  slice(1:185) %>% 
   gather("year", "trade",2:167) %>% 
-  filter(year>=1950, year<=2013, !is.na(trade)) %>% 
+  filter(year==2010, !is.na(trade)) %>% 
   mutate(trade=as.numeric(trade), year=as.numeric(year))
 
 country_code <- sapply(trade_data$Country, 
@@ -104,9 +72,9 @@ country_code <- sapply(trade_data$Country,
 trade_data$country_code <- country_code 
 trade_data <- trade_data[!is.na(trade_data$country_code),]
 
-# Madison data
-econ_data <- read_excel("data/mpd2018.xlsx", sheet="Full data") %>% 
-  filter(!is.na(cgdppc)) %>% filter(year>=1940) %>% 
+# PWT data
+econ_data <- read_excel("data/pwt90.xlsx", sheet="Data") %>% 
+  filter(year==2010) %>% 
   rename(country_code=countrycode)
 
 # Data on ports and harbors
@@ -114,24 +82,85 @@ region <- c("MAC","HKG","GRL","ALA","CUW","SXM","ABW","JEY","GGY","IMN")
 harbor_data <- countries_list@data %>% 
   mutate(country=SOVEREIGNT, country_code=ISO_A3, continent=CONTINENT) %>% 
   select(n_harbors, harbors, country, c_area, country_code, continent) %>% 
-  filter(!(country_code %in% region))
+  filter(!(country_code %in% region), country!="Northern Cyprus", country!="Kosovo")
 
 harbor_data[which(harbor_data$country=="Norway"),5] <- "NOR"
 harbor_data[which(harbor_data$country=="France"),5] <- "FRA"
 
 # Polity iv 
 polity_data <- read_excel("data/p4v2017.xls") %>% 
-  select(scode, country, year, polity, democ)
+  select(scode, country, year, polity2, democ) %>% filter(year==2010) 
 polity_data$country_code <- sapply(polity_data$country, 
         function(x) countrycode(x, 'country.name', 'iso3c'))
 
+# Landlocked countries:
+landlocked <- c(
+  "Afghanistan",	
+  "Andorra",
+  "Armenia",
+  "Austria",	
+  "Azerbaijan",
+  "Belarus",	
+  "Bhutan",	
+  "Bolivia",	
+  "Botswana",	
+  "Burkina Faso",	
+  "Burundi",	
+  "Central African Republic",	
+  "Chad",
+  "Czech Republic",	
+  "Eswatini", 
+  "Ethiopia",
+  "Hungary",	
+  "Kazakhstan",
+  "Kosovo",
+  "Kyrgyzstan",	
+  "Laos",	
+  "Lesotho",
+  "Liechtenstein",	
+  "Luxembourg",
+  "Macedonia",	
+  "Malawi",	
+  "Mali",	
+  "Moldova",	
+  "Mongolia",	
+  "Nepal",	
+  "Niger",	
+  "Paraguay",	
+  "Rwanda",	
+  "San Marino",	
+  "Serbia",	
+  "Slovakia",	
+  "South Ossetia",
+  "South Sudan",
+  "Switzerland",	
+  "Tajikistan",	
+  "Transnistria",
+  "Turkmenistan",
+  "Uganda",	
+  "Uzbekistan",
+  "Vatican City",
+  "West Bank",
+  "Zambia",	
+  "Zimbabwe")
+
+
 # Combining datasets
 combined <- inner_join(trade_data, harbor_data, by = "country_code") %>% 
-  inner_join(econ_data, by = c("country_code", "year")) %>%
-  inner_join(polity_data, by = c("country_code", "year")) %>% 
-  filter(n_harbors>0, year==2010) 
+  inner_join(econ_data, by = c("country_code")) %>%
+  inner_join(polity_data, by = c("country_code")) %>% 
+  filter(!is.na(polity2)) %>% select(country_code, country, year, harbors, n_harbors,
+                                rgdpe, pop, trade, c_area, polity2)  
+  
 
 
 
+
+
+
+
+
+
+#polity_data$country_code[!(polity_data$country_code %in% econ_data$country_code)]
 
 
