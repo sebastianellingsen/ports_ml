ed75370551173adb6dd9522dbf5b040be026a0b3
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-05-20 18:59:09 +0200

    preares the grid cell data

--- scripts/data_prep_gridcell.R
+++ scripts/data_prep_gridcell.R
@@ -1,49 +1,31 @@
 ## This file adds grid cell level data to the main dataset
 # load("~/Dropbox/ports_ml/output/data_prep_2_tmp1.RData")
 
-## Loading packages
-if (!require(pacman)) install.packages("pacman")
-p_load(tidyverse, 
-       sf, 
-       raster, 
-       tmap, 
-       sp, 
-       rgdal, 
-       rgeos, 
-       viridis, 
-       tmaptools,
-       spatialEco)
 
 ## Loading the main dataset
 # load("/Users/sebastianellingsen/Dropbox/ports_ml/africa.Rda")
-load("/Users/sebastianellingsen/Dropbox/ports_ml/output/my_work_space_24.RData")
+# load("/Users/sebastianellingsen/Dropbox/ports_ml/output/my_work_space_24.RData")
 
-## Loading, projecting, and aggregating raster files 
-newcrs <- CRS("+proj=moll +datum=WGS84 +units=km")
 
 ## Nighlights data
-lights <- raster("data/nightlights/F182010.v4/F182010.v4d_web.stable_lights.avg_vis.tif")
-lights_small <- aggregate(lights, 2)
-lights_small_projected <- projectRaster(lights_small,
-                                        crs = newcrs,
-                                        method = "bilinear")
+# lights <- raster("data/nightlights/F182010.v4/F182010.v4d_web.stable_lights.avg_vis.tif")
+# lights_small <- aggregate(lights, 2)
+# lights_small_projected <- projectRaster(lights_small,
+#                                         crs = newcrs,
+#                                         method = "bilinear")
 
-writeRaster(lights_small_projected, 
-            filename="lights_small_projected.grd", 
-            datatype='FLT4S', 
-            overwrite=TRUE)
 lights_small_projected <- raster("lights_small_projected.grd")
 
 ## Population density 
-pop_density <- raster("data/population_density/gpw-v4-population-density-adjusted-to-2015-unwpp-country-totals-rev10_2015_2pt5_min_tif/gpw_v4_population_density_adjusted_to_2015_unwpp_country_totals_rev10_2015_2pt5_min.tif")
-pop_density <- aggregate(pop_density, 2)
-pop_density_projected <- projectRaster(pop_density,
-                                       crs = newcrs,
-                                       method = "bilinear")
-writeRaster(pop_density_projected, 
-            filename="pop_density_projected.grd", 
-            datatype='FLT4S', 
-            overwrite=TRUE)
+# pop_density <- raster("data/population_density/gpw-v4-population-density-adjusted-to-2015-unwpp-country-totals-rev10_2015_2pt5_min_tif/gpw_v4_population_density_adjusted_to_2015_unwpp_country_totals_rev10_2015_2pt5_min.tif")
+# pop_density <- aggregate(pop_density, 2)
+# pop_density_projected <- projectRaster(pop_density,
+#                                        crs = newcrs,
+#                                        method = "bilinear")
+# writeRaster(pop_density_projected, 
+#             filename="pop_density_projected.grd", 
+#             datatype='FLT4S', 
+#             overwrite=TRUE)
 pop_density_projected <- raster("pop_density_projected.grd")
 
 ## Precipitation and climate variables
@@ -188,10 +170,71 @@ bio19 <- aggregate(bio19, 2)
 bio19_projected <- projectRaster(bio19, 
                                  crs = newcrs, 
                                  method = "bilinear")
-# Distance to mines
+# Distance to mineral deposit
 mines <- readOGR("data/mines/ofr20051294/ofr20051294.shp", 
                  "ofr20051294")
 mines <- spTransform(mines, newcrs)
+
+
+# Coffee suitabilty 
+coffee <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/coffee/res03_crav6190l_silr_cof.tif")
+coffee_projected <- projectRaster(coffee, 
+                                  crs = newcrs, 
+                                  method = "bilinear")
+
+# Tea suitabilty 
+tea <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/tea/res03_crav6190i_siir_tea.tif")
+tea_projected <- projectRaster(tea, 
+                               crs = newcrs, 
+                               method = "bilinear")
+
+# Tobacco suitabilty 
+tobacco <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/tobacco/res03_crav6190l_silr_tob.tif")
+tobacco_projected <- projectRaster(tobacco, 
+                               crs = newcrs, 
+                               method = "bilinear")
+
+# Banana suitabilty 
+banana <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/banana/res03_crav6190h_sihr_ban.tif")
+banana_projected <- projectRaster(banana, 
+                                   crs = newcrs, 
+                                   method = "bilinear")
+
+# Cacao suitabilty 
+cacao <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/cacao/res03_crav6190h_sihr_coc.tif")
+cacao_projected <- projectRaster(cacao, 
+                                 crs = newcrs, 
+                                 method = "bilinear")
+
+# Citrus suitabilty 
+citrus <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/citrus/res03_crav6190l_silr_cit.tif")
+citrus_projected <- projectRaster(citrus, 
+                                  crs = newcrs, 
+                                  method = "bilinear")
+
+# Cotton suitabilty 
+cotton <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/cotton/res03_crav6190l_silr_cot.tif")
+cotton_projected <- projectRaster(cotton, 
+                                  crs = newcrs, 
+                                  method = "bilinear")
+
+# Palm oil suitabilty 
+palm_oil <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/palm_oil/res03_crav6190l_silr_olp.tif")
+palm_oil_projected <- projectRaster(palm_oil, 
+                                  crs = newcrs, 
+                                  method = "bilinear")
+
+# Sugarcane suitabilty 
+sugarcane <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/sugarcane/res03_crav6190h_sihr_suc.tif")
+sugarcane_projected <- projectRaster(sugarcane, 
+                                    crs = newcrs, 
+                                    method = "bilinear")
+
+# Soybean suitabilty 
+soybean <- raster("/Users/sebastianellingsen/Dropbox/ports_ml/Data/crops/soybean/res03_crav6190i_siir_soy.tif")
+soybean_projected <- projectRaster(soybean, 
+                                     crs = newcrs, 
+                                     method = "bilinear")
 
 # Elevation data
 elev <- raster("elev.grd")
@@ -229,106 +272,118 @@ slope <- c()
 max_elev <- c()
 min_elev <- c()
 mean_elev <- c()
+coffee_distance <- c()
+tea_distance <- c()
+banana_distance <- c()
+tobacco_distance <- c()
+cacao_distance <- c()
+citrus_distance <- c()
+cotton_distance <- c()
+palm_oil_distance <- c()
+sugarcane_distance <- c()
+soybean_distance <- c()
+coast_distance <- c()
 mines_distance <- c()
 
 counter <- 0
 
+val <- function(layer, cell) {
+  val <- values(mask(crop(layer, cell), cell))
+  return(val)
+}
+
 for (i in row.names(africa@data)){
   cell <- africa[africa@data$ID==i,]
   
-  # Dependent variables
-  # lights_data[i] <- mean(values(crop(lights_small_projected,
-  #                                    cell)),
-  #                        na.rm=TRUE)
-  # density_data[i] <- mean(values(crop(pop_density_projected,
-  #                                     cell)),
-  #                         na.rm=TRUE)
+  ## Dependent variables
+  lights_data[i] <- mean(val(lights_small_projected, cell), na.rm=TRUE)
+  density_data[i] <- mean(val(pop_density_projected, cell), na.rm=TRUE)
   
-  # Terrain data
-  # ri <- tri(crop(elev, cell),
-  #           exact = TRUE,
-  #           s = 3)
-  # tri[i] <- mean(values(ri),
-  #                na.rm=TRUE)
-  # 
-  # slope_cell <- terrain(crop(elev, cell), 
-  #                       opt='slope', 
-  #                       unit='radians', 
-  #                       neighbors=8)
-  #   
-  # slope[i] <- mean(values(slope_cell),
-  #                na.rm=TRUE)
-  # 
-  # max_elev[i] <- max(values(crop(elev, cell)),
-  #                  na.rm=TRUE)
-  # 
-  # min_elev[i] <- min(values(crop(elev, cell)),
-  #                    na.rm=TRUE)
-  # 
-  # mean_elev[i] <- mean(values(crop(elev, cell)),
-  #                    na.rm=TRUE)
+  ## Terrain data
+  ri <- tri(crop(elev, cell),
+            exact = TRUE,
+            s = 3)
+  tri[i] <- mean(val(ri, cell), na.rm=TRUE)
+    
+  slope_cell <- terrain(crop(elev, cell),
+                        opt='slope',
+                        unit='radians',
+                        neighbors=8)
+
+  slope[i] <- mean(val(slope_cell, cell), na.rm=TRUE)
   
-  # Distance to mine
+  mean_elev[i] <- mean(val(elev, cell), na.rm=TRUE)
+  
+  min_elev[i] <- min(val(elev, cell), na.rm=TRUE)
+  
+  max_elev[i] <- max(val(elev, cell), na.rm=TRUE)
+  
+  ## Distance to mineral deposit
   mines_distance[i] <- gDistance(cell, mines)
   
-  # # Climate data
-  # bio1[i] <- mean(values(crop(bio1_projected, 
-  #                                    cell)), 
-  #                        na.rm=TRUE)
-  # bio2[i] <- mean(values(crop(bio2_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio3[i] <- mean(values(crop(bio3_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio4[i] <- mean(values(crop(bio4_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio5[i] <- mean(values(crop(bio5_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio6[i] <- mean(values(crop(bio6_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio7[i] <- mean(values(crop(bio7_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio8[i] <- mean(values(crop(bio8_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio9[i] <- mean(values(crop(bio9_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio10[i] <- mean(values(crop(bio10_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio11[i] <- mean(values(crop(bio11_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio12[i] <- mean(values(crop(bio12_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio13[i] <- mean(values(crop(bio13_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio14[i] <- mean(values(crop(bio14_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio15[i] <- mean(values(crop(bio15_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio16[i] <- mean(values(crop(bio16_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio17[i] <- mean(values(crop(bio17_projected, 
-  #                             cell)), 
-  #                 na.rm=TRUE)
-  # bio18[i] <- mean(values(crop(bio18_projected, 
-  #                              cell)), 
-  #                  na.rm=TRUE)
-  # bio19[i] <- mean(values(crop(bio19_projected, 
-  #                              cell)), 
-  #                  na.rm=TRUE)
+  ## Distance to coastline
+  coast_distance[i] <- gDistance(cell, coastline10)
+  
+  ## Climate data
+  bio1[i] <- mean(val(bio1_projected, cell), na.rm=TRUE)
+  
+  bio2[i] <- mean(val(bio2_projected, cell), na.rm=TRUE)
+  
+  bio3[i] <- mean(val(bio3_projected, cell), na.rm=TRUE)
+ 
+  bio4[i] <- mean(val(bio4_projected, cell), na.rm=TRUE)
+  
+  bio5[i] <- mean(val(bio5_projected, cell), na.rm=TRUE)
+
+  bio6[i] <- mean(val(bio6_projected, cell), na.rm=TRUE)
+  
+  bio7[i] <- mean(val(bio7_projected, cell), na.rm=TRUE)
+  
+  bio8[i] <- mean(val(bio8_projected, cell), na.rm=TRUE)
+
+  bio9[i] <- mean(val(bio9_projected, cell), na.rm=TRUE)
+
+  bio10[i] <- mean(val(bio10_projected, cell), na.rm=TRUE)
+  
+  bio11[i] <- mean(val(bio11_projected, cell), na.rm=TRUE)
+  
+  bio12[i] <- mean(val(bio12_projected, cell), na.rm=TRUE)
+
+  bio13[i] <- mean(val(bio13_projected, cell), na.rm=TRUE)
+  
+  bio14[i] <- mean(val(bio14_projected, cell), na.rm=TRUE)
+
+  bio15[i] <- mean(val(bio15_projected, cell), na.rm=TRUE)
+  
+  bio16[i] <- mean(val(bio16_projected, cell), na.rm=TRUE)
+
+  bio17[i] <- mean(val(bio17_projected, cell), na.rm=TRUE)
+
+  bio18[i] <- mean(val(bio18_projected, cell), na.rm=TRUE)
+  
+  bio19[i] <- mean(val(bio19_projected, cell), na.rm=TRUE)
+
+  ## Crop suitability data
+  
+  coffee_distance[i] <- mean(val(coffee_projected, cell), na.rm=TRUE)
+  
+  tea_distance[i] <- mean(val(tea_projected, cell), na.rm=TRUE)
+  
+  banana_distance[i] <- mean(val(banana_projected, cell), na.rm=TRUE)
+  
+  tobacco_distance[i] <- mean(val(tobacco_projected, cell), na.rm=TRUE)
+  
+  cacao_distance[i] <- mean(val(cacao_projected, cell), na.rm=TRUE)
+  
+  citrus_distance[i] <- mean(val(citrus_projected, cell), na.rm=TRUE)
+  
+  cotton_distance[i] <- mean(val(cotton_projected, cell), na.rm=TRUE)
+  
+  palm_oil_distance[i] <- mean(val(palm_oil_projected, cell), na.rm=TRUE)
+  
+  sugarcane_distance[i] <- mean(val(sugarcane_projected, cell), na.rm=TRUE)
+  
+  soybean_distance[i] <- mean(val(soybean_projected, cell), na.rm=TRUE)
   
   # Progress
   counter <- counter+1
@@ -336,10 +391,10 @@ for (i in row.names(africa@data)){
 }
 
 ## Joining and preparing datasets 
-
+       
 # Outcomes
-africa@data$lights_data <- lights_data
-africa@data$density_data <- density_data
+africa@data$lights <- lights_data
+africa@data$density <- density_data
 
 # Controls
 africa@data$tri <- tri
@@ -367,13 +422,23 @@ africa@data$slope <- slope
 africa@data$max_elev <- max_elev
 africa@data$min_elev <- min_elev
 africa@data$mean_elev <- mean_elev
-africa@data$mines_distance <- mines_distance
+africa@data$mine_dis <- mines_distance
+africa@data$coast_dis <- coast_distance
+africa@data$coffee_dis <- coffee_distance
+africa@data$tea_dis <- tea_distance
+africa@data$banana_dis <- banana_distance
+africa@data$tob_dis <- tobacco_distance
+africa@data$cacao_dis <- cacao_distance
+africa@data$citrus_dis <- citrus_distance
+africa@data$cotton_dis <- cotton_distance
+africa@data$palm_oil_dis <- palm_oil_distance
+africa@data$sugarcane_dis <- sugarcane_distance
+africa@data$soybean_dis <- soybean_distance
 
 ## Adding longitude and latitude
-# africa@data$long <- coordinates(africa)[,1]
-# africa@data$lat <- coordinates(africa)[,2]
+africa@data$long <- coordinates(africa)[,1]
+africa@data$lat <- coordinates(africa)[,2]
 
-# add slope tri and landcover data
 
 
 
