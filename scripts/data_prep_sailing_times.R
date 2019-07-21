ade70747dffd3e07356e78437e9c87d7d0281f64
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-07-21 11:54:36 +0200

    changed coordinates of ports to lie on the raster

--- scripts/data_prep_sailing_times.R
+++ scripts/data_prep_sailing_times.R
@@ -31,23 +31,38 @@ wind_series_mean_layer@layers[[2]] <- wind_series_mean_layer_masked
 
 # Obtaining the transition matrix
 Conductance <- flow.dispersion(wind_series_mean_layer, 
-                               output="transitionLayer")
+                               output="transitionLayer", 
+                               type="active")
 
 
-Alicante  <- c(-0.48149, 38.34517)
-Barcelona <- c(2.15899, 41.38879)
+# Spain
+Cadiz     <- c(-6.29465, 36.52978)
+Alicante  <- c(-0.48149, 38.04517)
+Barcelona <- c(2.26899, 41.38879)
 A_Coruña  <- c(-8.411540, 43.362343)
 
+# New world
+Havana         <- c(-82.366592, 23.313592)
+Veracruz       <- c(-96.134224, 19.173773)
+Cartagena      <- c(-75.51444, 10.39972)
+Lima           <- c(-77.042793, -12.846374)
+Panama         <- c(-79.516670, 8.583333)
+Acapulco       <- c(-99.912437, 16.448824)
+Rio_De_Janeiro <- c(-43.182365, -22.970722)
+Montevideo     <- c(-56.164532, -34.901112)
+Buenos_Aires   <- c(-58.081592, -34.603722)
+
 ## Port locations
-loc <- matrix(c(-6.29465,    -0.48149,      2.15899, 
-                -8.411540,   -82.366592,  -96.134224, 
-                -75.51444,   -77.042793,  -79.516670, 
-                -99.912437,  -43.182365,  -56.164532, 
-                -58.381592,   36.52978,    38.34517,   
-                 41.38879,    43.362343,   23.113592,  
-                 19.173773,   10.39972,   -12.046374,   
-                  8.983333,   16.848824,  -22.970722, 
-                -34.901112,  -34.603722), 
+loc <- matrix(c(Cadiz[1],       Alicante[1],       Barcelona[1], 
+                A_Coruña[1],    Havana[1],         Veracruz[1], 
+                Cartagena[1],   Lima[1],           Panama[1],   
+                Acapulco[1],    Rio_De_Janeiro[1], Montevideo[1], 
+                Buenos_Aires[1],
+                Cadiz[2],       Alicante[2],       Barcelona[2], 
+                A_Coruña[2],    Havana[2],         Veracruz[2], 
+                Cartagena[2],   Lima[2],           Panama[2], 
+                Acapulco[2],    Rio_De_Janeiro[2], Montevideo[2], 
+                Buenos_Aires[2]), 
               13, 2)
 
 colnames(loc) <- c("lon", "lat")
@@ -71,24 +86,31 @@ least_cost_path <-  function(x, y){
   return(list(path, cost))
 }
 
+
 ## Costs and paths from Spanish ports
 # From Cadiz 
 shortest_path_coruna <- lapply(5:13, 
                                function(x) least_cost_path(1, x))
-# From Alicante 
-shortest_path_coruna <- lapply(5:13, 
-                               function(x) least_cost_path(2, x))
-# From Barcelona 
-shortest_path_coruna <- lapply(5:13, 
-                               function(x) least_cost_path(3, x))
-# From A Coruña 
-shortest_path_coruna <- lapply(5:13, 
-                               function(x) least_cost_path(4, x))
+
+
+## Costs and paths from Havana
+shortest_path_havana <- lapply(1:4, 
+                               function(x) least_cost_path(x, 5))
+
+path <- shortestPath(Conductance,
+                     loc[5,],
+                     loc[4,], 
+                     output = "SpatialLines")
 
 
 
-
-
+# pass pa at ports er pa raster, gjelder lima, funker ofte bare en vei ellers
+# 
+# "Veracruz", 
+#                    "Cartagena", "Lima", "Panama", 
+# "Acapulco", "Rio_De_Janeiro", 
+# "Montevideo", "Buenos_Aires"
+# 
 
 
 
