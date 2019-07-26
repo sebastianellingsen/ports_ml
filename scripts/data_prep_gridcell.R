a41f0731b2e7ffe162db6854300635a34883cbbc
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-07-26 12:47:49 +0200

    added section to impute missing values with the nearest neighbors

--- scripts/data_prep_gridcell.R
+++ scripts/data_prep_gridcell.R
@@ -54,24 +54,82 @@ rasters  <- c(bio1,    bio2,   bio3,  bio4,  bio5,  bio6,   bio7,
 extracting_raster_info <- function(x){
   
   cell             <- south_america[south_america@data$ID==x,]
-  cell_values      <- sapply(rasters, function(raster) raster::extract(raster, cell))
+  cell_values      <- sapply(rasters, 
+                             function(raster) raster::extract(raster, cell))
   return(sapply(cell_values, function(x) mean(x, na.rm=TRUE)))
 
 }
 
 # Adding the information from the raster to dataframe
-tic()
-values         <- lapply(south_america@data$ID, function(x) extracting_raster_info(x))
-controls       <- as.data.frame(do.call(rbind, values))
+values          <- lapply(south_america@data$ID, 
+                         function(x) extracting_raster_info(x))
+controls        <- as.data.frame(do.call(rbind, values))
 names(controls) <- c("bio1",  "bio2",  "bio3",    "bio4",   "bio5",   "bio6", 
                      "bio7",  "bio8",  "bio9",    "bio10",  "bio11",  "bio12", 
                      "bio13", "bio14", "bio15",   "bio16",  "bio17",  "bio18", 
                      "bio19", "bio20", "coffee",  "tobaco", "cotton", "sugar")
 
-toc()
-
 # Adding the dataframe to the grid 
 south_america@data <- cbind(south_america@data, controls)
+
+
+## Imputing missing values 
+
+# Finding the missing cells  
+missing_rows <- south_america[apply(is.na(south_america@data), 1, any),]$ID
+
+# This function takes the nearest cells of the missing cell and imputed the 
+# control with a simple average 
+
+neighbors_imputation <- function(x){
+  
+  # Accessing the cell with missing data 
+  cell <- south_america[south_america@data$ID==x,]
+  
+  # Calculating the distance to other cells
+  gd <-  gDistance(south_america,
+                   cell,
+                   byid=TRUE)
+  
+  # Finding the closest cells 
+  distances1 <- cbind("ID"=colnames(gd), "distance"=gd[1:dim(gd)[2]]) %>% 
+    as.data.frame(stringsAsFactors = FALSE) %>% 
+    mutate(distance=as.numeric(distance)) %>% 
+    arrange(distance) %>% 
+    top_n(-7) %>% 
+    filter(ID!=rownames(gd))
+  
+  # Neighboring cells 
+  neighbors <- south_america[south_america@data$ID %in% distances1$ID, ]
+  
+  # Imputing the missing values
+  missing_rows_number <- which(south_america@data$ID==x)
+  missing_variable    <- which(is.na(cell@data)) 
+  data_for_imputation <- neighbors@data[,missing_variable] %>% as.data.frame()
+  imputed_value       <- apply(data_for_imputation, 2, mean, na.rm=TRUE) 
+  
+  return(cbind(missing_rows_number, missing_variable, imputed_value))
+}
+
+# Extracting a list of imputed values
+imputed_list <- lapply(missing_rows,
+                       function(x) neighbors_imputation(x))
+
+# Adding imputed values to the missing cells 
+for(i in 1:114){
+  missing_rows_number <- imputed_list[[i]][1]
+  missing_variable <- imputed_list[[i]][,2]
+  imputed_value <- imputed_list[[i]][,3]
+  
+  south_america@data[missing_rows_number, missing_variable] <- imputed_value
+}
+
+
+## Adding dummy variable for coastal areas
+
+
+
+
 
 
 
