d0882c5aca2e3068f0ae9efb8ae386a99de937d2
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-10-11 10:44:12 +0200

    removed section defining the polynomials

--- scripts/data_prep_2.R
+++ scripts/data_prep_2.R
@@ -19,19 +19,68 @@ p_load(tidyverse,
        spatialEco, 
        ranger, 
        caret,
-       lfe)
+       lfe,
+       glmnet,
+       broom)
 
 newcrs <- CRS("+proj=moll +datum=WGS84 +units=km")
 bwidth <- 3
 
-## Ports 
+countries_list <- c("CI", 
+                    "PE", 
+                    "AR", 
+                    "UY",   
+                    "EC", 
+                    "CO", 
+                    "VE", 
+                    "PA",
+                    "SV", 
+                    "HN", 
+                    "CR", 
+                    "GT", 
+                    "MX", 
+                    "NI", 
+                    "CU", 
+                    "DO")
+
+## Loading ports datasets
+
+# Ports WPI
 ports <- readOGR("data/WPI_Shapefiles/WPI_Shapefile2010", "WPI")
 ports <- ports[!is.na(ports$HARBORTYPE),]
 ports <- ports[ports$HARBORTYPE=="CN",]
+ports <- ports[!(ports$COUNTRY%in%countries_list),]
+
+ports <- ports@data %>%   
+  rename(lat = LATITUDE, lon = LONGITUDE) %>% 
+  dplyr::select(lat, lon)
+
+# Historical ports datasets
+hist_ports1 <- read_xlsx("data/historical_ports/AncientPorts.xlsx", 
+                        sheet="PLACES") %>% 
+  rename(lat = LATITUDE, lon = LONGITUDE) %>% 
+  dplyr::select(lat, lon) %>% 
+  filter(!is.na(lat), !is.na(lon))
+
+hist_ports2 <- read_xlsx("data/historical_ports/AncientPorts.xlsx", 
+                        sheet="Potential Harb", skip=2) %>% 
+  rename(lat = LATITUDE, lon = LONGITUDE) %>% 
+  dplyr::select(lat, lon) %>% 
+  filter(!is.na(lat), !is.na(lon))
+
+# Making a spatial dataframe 
+ports <- rbind(ports, hist_ports1, hist_ports2)
+xy    <- cbind(ports$lon, ports$lat)
+ports <- SpatialPoints(coords = xy, 
+                       proj4string = crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
 
 
 ## Coastline
 coastline10 <- ne_download(scale = 10, 
+                           type = 'coastline', 
+                           category = 'physical')
+
+coastline110 <- ne_download(scale = 110, 
                            type = 'coastline', 
                            category = 'physical')
 
@@ -50,7 +99,12 @@ rivers50 <- ne_download(scale = 50,
                         category = 'physical')
 
 ## Elevation and terrain ruggedness data
-elev       <- raster("data/prepared_rasters/elev.grd")
+elev <- raster("data/elevation/ETOPO1_Ice_g_geotiff.tif")
+crs(elev) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+elev <- projectRaster(elev, 
+                      crs    = newcrs, 
+                      method = "bilinear")
+
 tri        <- raster("data/prepared_rasters/tri.grd")
 
 slope <- terrain(elev, 
@@ -62,6 +116,7 @@ slope <- terrain(elev,
 countries10  <- spTransform(countries10, newcrs)
 countries110 <- spTransform(countries110, newcrs)
 coastline10  <- spTransform(coastline10, newcrs)
+coastline110 <- spTransform(coastline110, newcrs)
 rivers50     <- spTransform(rivers50, newcrs)
 ports        <- spTransform(ports, newcrs)
 
@@ -72,18 +127,18 @@ ports        <- spTransform(ports, newcrs)
 ################################################################################
 
 ## Removing small countries
-small <- c("Vanatu", 
-           "San Marino", 
-           "Vatican", 
-           "Fiji", 
-           "Solomon Islands",
-           "Federated States of Micronesia", 
-           "Palau", 
-           "Samoa", 
-           "Nauru")
+# small <- c("Vanatu", 
+#            "San Marino", 
+#            "Vatican", 
+#            "Fiji", 
+#            "Solomon Islands",
+#            "Federated States of Micronesia", 
+#            "Palau", 
+#            "Samoa", 
+#            "Nauru")
 
-study_area <- countries10[!(countries10$SOVEREIGNT %in% small),]
-
+# study_area <- countries10[!(countries10$SOVEREIGNT %in% small),]
+study_area <- countries10
 
 ## Extracting values
 
@@ -93,8 +148,8 @@ study_area <- countries10[!(countries10$SOVEREIGNT %in% small),]
 a <- gBuffer(ports, width = bwidth, byid=TRUE)
 
 # Elevation in buffer
-b <- raster::extract(elev, a)
-tri_port <- raster::extract(elev, a)
+b           <- raster::extract(elev, a)
+tri_port    <- raster::extract(elev, a)
 slope_port  <- raster::extract(slope, a)
 
 # Elevation 
@@ -130,7 +185,7 @@ mean_slope<- sapply(1:length(slope_port),
                         function(x) mean(slope_port[[x]],
                                          na.rm = TRUE))
 
-## Slope at the port
+## Indicator for port
 y <- rep(1, length(slope_port)[1])
 
 # Adding continent
@@ -138,8 +193,8 @@ cont <- over(a, countries10)$SUBREGION
 cont <- ifelse(is.na(cont), "na", cont)
 
 # Measuring distance to river
-river_dist <- sapply(a@data$INDEX_NO, 
-                     function(i) gDistance(a[a@data$INDEX_NO==i,], rivers50))
+river_dist <- sapply(1:length(a), 
+                     function(i) gDistance(a[i], rivers50))
 
 
 ## Sampling points on the the coastline
@@ -149,29 +204,21 @@ sample_coastline <- spsample(coastline10,
                              method="Line")
 sample_coastline <- sample_coastline[sample(length(sample_coastline)),] 
 
-
-## Calculating the closest sampled point to each port
-port_on_coastline <- c()
-len               <- c()
-k                 <- 1
-for (i in ports@data$INDEX_NO){
-  
-  point <- ports[ports@data$INDEX_NO==i,]
-  
+## Calculating the length of the coastline 
+coastline_length <- function(x){
   ## Finding the closest on point on the coastline
-  gd                <-  gDistance(sample_coastline, 
-                                  point, 
+  gd                <-  gDistance(sample_coastline,
+                                  ports[x],
                                   byid=TRUE)
-
   port_on_coastline <-  sample_coastline[apply(gd, 1, which.min)]
-
+ 
   ## Measuring the coastal indentation
   port_on_coastline  <-  gBuffer(port_on_coastline, width = 3)
-  len[k]             <-   rgeos::gLength(crop(coastline10, port_on_coastline))
-  
-  k <- k+1
-  print(k/length(ports@data$INDEX_NO))
+  return(rgeos::gLength(crop(coastline10, port_on_coastline)))
 }
+
+coastline_length <- sapply(1:length(ports), 
+                           function(x) coastline_length(x))
 
 
 ## Combining the data 
@@ -184,17 +231,19 @@ port_df <- cbind(mean_slope,
                  tri_p,
                  slope_p,
                  cont,
-                 len,
+                 coastline_length,
                  river_dist,
                  y)
 
 
 sample <- spsample(coastline10, 
-                   5000, 
+                   10000, 
                    type = "random", 
                    method="Line")
-sample <- sample[sample(length(sample)),] 
-sample <- sample[1:dim(ports@data)[1]]
+
+# Randomize the order of the points
+sample <- sample[sample(length(sample)),]
+sample <- sample[1:dim(port_df)[1]]
 
 ## Sampled locations 
 # Buffer around each point
@@ -250,13 +299,13 @@ cont <- over(a, countries10)$SUBREGION
 cont <- ifelse(is.na(cont), "na", cont)
 
 # Measuring distance to river
-river_dist <- sapply(1:length(a@polygons), 
-                     function(i)gDistance(a[i], rivers50))
+river_dist <- sapply(1:length(a), 
+                     function(i) gDistance(a[i], rivers50))
 
 # Adding information on the coastline length
-len <- sapply(1:length(a@polygons), 
-              function(i) rgeos::gLength(crop(coastline10, a[i])))
 
+coastline_length <- sapply(1:length(a), 
+                           function(x) coastline_length(x))
 
 ## Combining the data 
 no_port_df <- cbind(mean_slope, 
@@ -268,89 +317,28 @@ no_port_df <- cbind(mean_slope,
                     tri_p,
                     slope_p,
                     cont,
-                    len,
+                    coastline_length,
                     river_dist,
                     y)
 
 ## Generating dataframe for prediction
 pred_dataframe <- rbind(port_df, no_port_df) %>% 
   data.frame() %>%
-  mutate(y=as.factor(y),
-         mean_slope=as.numeric(as.character(mean_slope)), 
-         mean_tri=as.numeric(as.character(mean_tri)), 
-         min_elev=as.numeric(as.character(min_elev)),
-         max_elev=as.numeric(as.character(max_elev)), 
-         max_elev=as.numeric(as.character(max_elev)), 
-         len=as.numeric(as.character(len)), 
-         river_dist=as.numeric(as.character(river_dist)), 
-         mean_elev=as.numeric(as.character(mean_elev)),
-         elevation_p=as.numeric(as.character(elevation_p)),
-         tri_p=as.numeric(as.character(tri_p)),
-         slope_p=as.numeric(as.character(slope_p)))
+  mutate(y                        = as.factor(y),
+         mean_slope               = as.numeric(as.character(mean_slope)), 
+         mean_tri                 = as.numeric(as.character(mean_tri)), 
+         min_elev                 = as.numeric(as.character(min_elev)),
+         max_elev                 = as.numeric(as.character(max_elev)), 
+         max_elev                 = as.numeric(as.character(max_elev)), 
+         coastline_length         = as.numeric(as.character(coastline_length)), 
+         river_dist               = as.numeric(as.character(river_dist)), 
+         mean_elev                = as.numeric(as.character(mean_elev)),
+         elevation_p              = as.numeric(as.character(elevation_p)),
+         tri_p                    = as.numeric(as.character(tri_p)),
+         slope_p                  = as.numeric(as.character(slope_p)))
 
-## Adding polynomials
-d <- c()
-names <- colnames(pred_dataframe)[1:11]
-for(i in 2:5) { 
-  
-  k <- sapply(1:11, function(x) pred_dataframe[,x]^i) 
-  
-  colnames(k) <- paste(names[1:11], i, sep = "")
-  
-  d <- cbind(d, k)
-}
-polynomials <- d %>% as.data.frame() %>%  
-  dplyr::select(-c(cont2, cont3, cont4, cont5))
+## Saving file as a csv
+pred_dataframe1 <- pred_dataframe[sample(dim(pred_dataframe)[1]), ]
+write.csv(pred_dataframe, "output/pred_dataframe.csv", row.names = FALSE)
 
-
-## Adding interaction terms
-inter1           <- sapply(2:11, 
-                           function(x) pred_dataframe[,1]*pred_dataframe[,x])
-colnames(inter1) <- paste0("mean_slope", colnames(pred_dataframe)[2:11])
-
-inter2           <- sapply(3:11, 
-                           function(x) pred_dataframe[,2]*pred_dataframe[,x])
-colnames(inter2) <- paste0("mean_tri", colnames(pred_dataframe)[3:11])
-
-inter3           <- sapply(4:11, 
-                           function(x) pred_dataframe[,3]*pred_dataframe[,x])
-colnames(inter3) <- paste0("min_elev", colnames(pred_dataframe)[4:11])
-
-inter4           <- sapply(5:11, 
-                           function(x) pred_dataframe[,4]*pred_dataframe[,x])
-colnames(inter4) <- paste0("max_elev", colnames(pred_dataframe)[5:11])
-
-inter5           <- sapply(6:11, 
-                           function(x) pred_dataframe[,5]*pred_dataframe[,x])
-colnames(inter5) <- paste0("mean_elev", colnames(pred_dataframe)[6:11])
-
-inter6           <- sapply(7:11, 
-                           function(x) pred_dataframe[,6]*pred_dataframe[,x])
-colnames(inter6) <- paste0("elevation_p", colnames(pred_dataframe)[7:11])
-
-inter7           <- sapply(8:11, 
-                           function(x) pred_dataframe[,7]*pred_dataframe[,x])
-colnames(inter7) <- paste0("tri_p", colnames(pred_dataframe)[8:11])
-
-inter8           <- sapply(11:11, 
-                           function(x) pred_dataframe[,11]*pred_dataframe[,x])
-colnames(inter8) <- paste0("len", colnames(pred_dataframe)[11:11])
-
-
-interactions <- cbind(inter1, inter2, inter3, inter4, inter5, 
-                       inter6, inter7, inter8) %>% 
-                as.data.frame() %>% 
-                dplyr::select(-c(mean_tricont, tri_pcont, elevation_pcont, 
-                                 mean_elevcont, max_elevcont, min_elevcont, 
-                                 mean_tricont, mean_slopecont))
-
-## Adding dummy variables
-dummies         <-  model.matrix(~pred_dataframe$cont) %>% as.data.frame()
-
-## Joining the datasets
-pred_dataframe1 <- cbind(pred_dataframe, polynomials, interactions, dummies)%>% 
-  filter(!is.na(slope_p))
-
-## Resampling the order of the rows
-pred_dataframe1 <- pred_dataframe1[sample(nrow(pred_dataframe1)),] 
 
