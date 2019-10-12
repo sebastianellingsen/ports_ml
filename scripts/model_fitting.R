8ef2db7818493c121953c0cb3be211a4f0d48214
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-10-12 19:27:05 +0200

    added a random forest model

--- scripts/model_fitting.R
+++ scripts/model_fitting.R
@@ -1,23 +1,25 @@
 ## This file samples points and fits the model 
 
-sa_countries <- c("Argentina",
-                  "Uruguay",
-                  "Paraguay",
-                  "Chile",
-                  "Bolivia",
-                  "Peru",
-                  "Colombia",
-                  "Venezuela",
-                  "Ecuador",
-                  "Brazil",
-                  "Panama",
-                  "Costa Rica",
-                  "Nicaragua",
-                  "Honduras",
-                  "Guatemala",
-                  "Mexico")
+countries_list <- c("Chile", 
+                    "Bolivia", 
+                    "Peru", 
+                    "Argentina", 
+                    "Uruguay",   
+                    "Ecuador", 
+                    "Colombia", 
+                    "Paraguay", 
+                    "Venezuela", 
+                    "Panama",
+                    "El Salvador", 
+                    "Honduras", 
+                    "Costa Rica", 
+                    "Guatemala", 
+                    "Mexico", 
+                    "Nicaragua", 
+                    "Cuba", 
+                    "Dominican Republic")
 
-study_area <- countries10[countries10@data$ADMIN %in% sa_countries, ]
+study_area <- countries10[countries10@data$ADMIN %in% countries_list, ]
 buffer <- gBuffer(study_area, width = 100)
 coastline_study_area <- gIntersection(coastline10, buffer)
 
@@ -26,13 +28,22 @@ sample <- spsample(coastline_study_area,
                    type = "random", 
                    method="Line")
 sample <- sample[sample(length(sample)),] 
+# 
+# ## Elevation and terrain ruggedness data
+# elev      <- raster("data/elevation/ETOPO1_Ice_g_geotiff.tif")
+# crs(elev) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+# elev      <- projectRaster(elev, 
+#                            crs    = newcrs, 
+#                            method = "bilinear")
+# 
+# tri   <- raster("data/prepared_rasters/tri.grd")
+# slope <- terrain(elev, 
+#                  opt='slope', 
+#                  unit='radians', 
+#                  neighbors=8)
+#
 
-elev       <- raster("data/prepared_rasters/elev.grd")
-tri        <- raster("data/prepared_rasters/tri.grd")
-slope      <- terrain(elev, 
-                      opt='slope', 
-                      unit='radians', 
-                      neighbors=8)
+
 
 # Buffer around each point
 a <- gBuffer(sample, width = bwidth, byid=TRUE)
@@ -94,11 +105,11 @@ cont <- ifelse(is.na(cont), "na", cont)
 
 
 ## Adding information on the coastline length
-len <- c()
+coastline_length <- c()
 k   <- 1
 for (i in 1:length(a@polygons)){
   
-  len[i] <- rgeos::gLength(crop(coastline10, a[i]))
+  coastline_length[i] <- rgeos::gLength(crop(coastline10, a[i]))
   k <- k+1
 }
 
@@ -112,107 +123,21 @@ sample_df <- cbind(mean_slope,
                    tri_p,
                    slope_p,
                    cont,
-                   len,
+                   coastline_length,
                    river_dist) %>% 
               data.frame() 
 
 sample_df <- sample_df %>% 
-  mutate(mean_slope=as.numeric(as.character(mean_slope)), 
-         mean_tri=as.numeric(as.character(mean_tri)), 
-         min_elev=as.numeric(as.character(min_elev)),
-         max_elev=as.numeric(as.character(max_elev)), 
-         max_elev=as.numeric(as.character(max_elev)), 
-         len=as.numeric(as.character(len)), 
-         river_dist=as.numeric(as.character(river_dist)), 
-         mean_elev=as.numeric(as.character(mean_elev)),
-         elevation_p=as.numeric(as.character(elevation_p)),
-         tri_p=as.numeric(as.character(tri_p)),
-         slope_p=as.numeric(as.character(slope_p)))
-
-
-## Adding polynomials
-d <- c()
-for(i in 2:5) { 
-  
-  k <- sapply(1:11, function(x) sample_df[,x]^i)
-  
-  colnames(k) <- paste(names[1:11], i, sep = "")
-  
-  d <- cbind(d, k)
-}
-polynomials <- d %>% as.data.frame() %>%  
-  dplyr::select(-c(cont2, cont3, cont4, cont5))
-
-
-## Adding interaction terms
-inter1           <- sapply(2:11, 
-                           function(x) sample_df[,1]*sample_df[,x])
-colnames(inter1) <- paste0("mean_slope", colnames(sample_df)[2:11])
-
-inter2           <- sapply(3:11, 
-                           function(x) sample_df[,2]*sample_df[,x])
-colnames(inter2) <- paste0("mean_tri", colnames(sample_df)[3:11])
-
-inter3           <- sapply(4:11, 
-                           function(x) sample_df[,3]*sample_df[,x])
-colnames(inter3) <- paste0("min_elev", colnames(sample_df)[4:11])
-
-inter4           <- sapply(5:11, 
-                           function(x) sample_df[,4]*sample_df[,x])
-colnames(inter4) <- paste0("max_elev", colnames(sample_df)[5:11])
-
-inter5           <- sapply(6:11, 
-                           function(x) sample_df[,5]*sample_df[,x])
-colnames(inter5) <- paste0("mean_elev", colnames(sample_df)[6:11])
-
-inter6           <- sapply(7:11, 
-                           function(x) sample_df[,6]*sample_df[,x])
-colnames(inter6) <- paste0("elevation_p", colnames(sample_df)[7:11])
-
-inter7           <- sapply(8:11, 
-                           function(x) sample_df[,7]*sample_df[,x])
-colnames(inter7) <- paste0("tri_p", colnames(sample_df)[8:11])
-
-inter8           <- sapply(11:11, 
-                           function(x) sample_df[,11]*sample_df[,x])
-colnames(inter8) <- paste0("len", colnames(sample_df)[11:11])
-
-
-interactions <- cbind(inter1, inter2, inter3, inter4, inter5, 
-                        inter6, inter7, inter8) %>% 
-                as.data.frame() %>% 
-                dplyr::select(-c(mean_tricont, tri_pcont, elevation_pcont, 
-                                 mean_elevcont, max_elevcont, min_elevcont, 
-                                 mean_tricont, mean_slopecont))
-
-
-## Adding dummy variables
-dummies         <-  model.matrix(~sample_df$cont) %>% as.data.frame()
-pred_dataframe3 <- cbind(sample_df, polynomials, interactions, dummies) %>% 
-  mutate("pred_dataframe$contAustralia and New Zealand" = 0,
-         "pred_dataframe$contCaribbean"                 = 0,                
-         "pred_dataframe$contCentral Asia"              = 0,         
-         "pred_dataframe$contEastern Africa"            = 0,       
-         "pred_dataframe$contEastern Asia"              = 0,             
-         "pred_dataframe$contEastern Europe"            = 0,           
-         "pred_dataframe$contMelanesia"                 = 0,                
-         "pred_dataframe$contMicronesia"                = 0,               
-         "pred_dataframe$contMiddle Africa"             = 0,            
-         "pred_dataframe$contna"                        = 0,                       
-         "pred_dataframe$contNorthern Africa"           = 0,          
-         "pred_dataframe$contNorthern Europe"           = 0,          
-         "pred_dataframe$contPolynesia"                 = 0,                
-         "pred_dataframe$contSeven seas (open ocean)"   = 0,
-         "pred_dataframe$contSouth-Eastern Asia"        = 0,       
-         "pred_dataframe$contSouthern Africa"           = 0,          
-         "pred_dataframe$contSouthern Asia"             = 0,            
-         "pred_dataframe$contSouthern Europe"           = 0,          
-         "pred_dataframe$contWestern Africa"            = 0,           
-         "pred_dataframe$contWestern Asia"              = 0)
-
-
-
-sample_df <- pred_dataframe3
+  mutate(mean_slope       = as.numeric(as.character(mean_slope)), 
+         mean_tri         = as.numeric(as.character(mean_tri)), 
+         min_elev         = as.numeric(as.character(min_elev)),
+         max_elev         = as.numeric(as.character(max_elev)), 
+         coastline_length = as.numeric(as.character(coastline_length)), 
+         river_dist       = as.numeric(as.character(river_dist)), 
+         mean_elev        = as.numeric(as.character(mean_elev)),
+         elevation_p      = as.numeric(as.character(elevation_p)),
+         tri_p            = as.numeric(as.character(tri_p)),
+         slope_p          = as.numeric(as.character(slope_p)))
 
 
 ## Adding the attributes to the shapefile 
@@ -227,20 +152,13 @@ sps_df_tmp <- SpatialPolygonsDataFrame(a,
 ## Fit model on the prediction dataset
 sample_df <- sps_df_tmp@data
 
-y <- pred_dataframe1[,12]
-x <- data.matrix(pred_dataframe1[,-12])
+model <- ranger(formula= as.numeric(y)~.,
+                data=pred_dataframe,
+                num.trees = 1000,
+                mtry = 5)
+prediction <- predict(model, sample_df)$predictions
+prediction <- ifelse(prediction>0.7,1,0)
 
-fit <- glmnet(x, 
-             y, 
-             family = "binomial", 
-             standardize = T)
-
-cvfit = cv.glmnet(x, y, family = "binomial", type.measure = "class")
-lambda.min <- cvfit$lambda.min
-prediction <- predict(fit, 
-                      data.matrix(sample_df), 
-                      type = "response", 
-                      s = lambda.min)
 sample_df$prediction <- prediction 
 sample_df <- sample_df[order(-prediction),] 
 # sample_df1 <- sample_df[1:93,]
@@ -253,9 +171,10 @@ predicted_ports <- sps_df_tmp[rownames(sps_df_tmp@data)%in%ID,]
 predicted_ports <- SpatialPolygonsDataFrame(predicted_ports, 
                                             sample_df1, 
                                             match.ID = TRUE)
-# predicted_ports <- predicted_ports[]
+predicted_ports <- predicted_ports[predicted_ports@data$prediction==1,]
 predicted_ports@data$prediction <- as.numeric(predicted_ports@data$prediction)
 
 
 
-
+tm_shape(coastline_study_area)+tm_lines(alpha=0.5)+
+  tm_shape(predicted_ports)+tm_dots()
