7fae3c1bf2a1a3c3ca91d2d83c8760d613e64524
sebastianellingsen <sebastian.ellingsen@upf.edu>
2019-10-28 18:25:03 +0100

    added section to extract the least likely locations

--- scripts/model_fitting.R
+++ scripts/model_fitting.R
@@ -28,21 +28,6 @@ sample <- spsample(coastline_study_area,
                    type = "random", 
                    method="Line")
 sample <- sample[sample(length(sample)),] 
-# 
-# ## Elevation and terrain ruggedness data
-# elev      <- raster("data/elevation/ETOPO1_Ice_g_geotiff.tif")
-# crs(elev) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-# elev      <- projectRaster(elev, 
-#                            crs    = newcrs, 
-#                            method = "bilinear")
-# 
-# tri   <- raster("data/prepared_rasters/tri.grd")
-# slope <- terrain(elev, 
-#                  opt='slope', 
-#                  unit='radians', 
-#                  neighbors=8)
-#
-
 
 
 # Buffer around each point
@@ -83,7 +68,13 @@ mean_slope <- sapply(1:length(slope_sample),
                      function(x) mean(slope_sample[[x]],
                                       na.rm = TRUE))
 
-y <- rep(0, length(slope_sample)[1])
+layer <- raster::extract(wind, a)
+wind_p <- sapply(1:length(layer), 
+                 function(x) mean(layer[[x]],
+                                  na.rm = TRUE))
+# Imputing 23 NAs with the mean value 
+wind_p <- ifelse(is.na(wind_p), mean(wind_p, na.rm=T), wind_p)
+
 
 
 ## Adding continent and distance to closest river 
@@ -114,30 +105,29 @@ for (i in 1:length(a@polygons)){
 }
 
 # Joining the data
-sample_df <- cbind(mean_slope, 
-                   mean_tri, 
-                   min_elev,
+sample_df <- cbind(min_elev,
                    max_elev, 
-                   mean_elev,
                    elevation_p,
                    tri_p,
-                   slope_p,
                    cont,
+                   slope_p,
                    coastline_length,
+                   wind_p,
                    river_dist) %>% 
               data.frame() 
 
 sample_df <- sample_df %>% 
-  mutate(mean_slope       = as.numeric(as.character(mean_slope)), 
-         mean_tri         = as.numeric(as.character(mean_tri)), 
-         min_elev         = as.numeric(as.character(min_elev)),
+  mutate(min_elev         = as.numeric(as.character(min_elev)),
          max_elev         = as.numeric(as.character(max_elev)), 
-         coastline_length = as.numeric(as.character(coastline_length)), 
+         coastline_length = as.numeric(as.character(coastline_length)),
          river_dist       = as.numeric(as.character(river_dist)), 
-         mean_elev        = as.numeric(as.character(mean_elev)),
+         wind_p           = as.numeric(as.character(wind_p)), 
          elevation_p      = as.numeric(as.character(elevation_p)),
          tri_p            = as.numeric(as.character(tri_p)),
-         slope_p          = as.numeric(as.character(slope_p)))
+         slope_p          = as.numeric(as.character(slope_p))) %>% 
+  filter(!is.na(slope_p))
+
+write.csv(sample_df, "output/sample_df.csv", row.names = FALSE)
 
 
 ## Adding the attributes to the shapefile 
@@ -147,34 +137,59 @@ row.names(sample_df) <- ID
 sps_df_tmp <- SpatialPolygonsDataFrame(a, 
                                        sample_df, 
                                        match.ID = TRUE)
+# writeOGR(sps_df_tmp,
+#          ".",
+#          "output/sps_df_tmp",
+#          driver="ESRI Shapefile",
+#          overwrite=TRUE)
+
+## Read the files
+sps_df_tmp     <- readOGR("output/sps_df_tmp.shp", "sps_df_tmp")
+colnames(sps_df_tmp@data) <- c('min_elev',
+                               'max_elev',
+                               'elevation_p',
+                               'tri_p',
+                               'cont',
+                               'slope_p',
+                               'coastline_length',
+                               'wind_p',
+                               'river_dist')
+sps_df_tmp@data <- sps_df_tmp@data[,-5]
+
+pred_dataframe <- read_csv("output/pred_dataframe.csv")
+
 
 
 ## Fit model on the prediction dataset
 sample_df <- sps_df_tmp@data
-
 model <- ranger(formula= as.numeric(y)~.,
                 data=pred_dataframe,
                 num.trees = 1000,
                 mtry = 5)
-prediction <- predict(model, sample_df)$predictions
-prediction <- ifelse(prediction>0.7,1,0)
+prediction_p <- predict(model, sample_df)$predictions
+prediction   <- ifelse(prediction_p>0.85,1,0)
+prediction   <- ifelse(prediction_p<0.065,1,0)
 
 sample_df$prediction <- prediction 
 sample_df <- sample_df[order(-prediction),] 
 # sample_df1 <- sample_df[1:93,]
 sample_df1 <- sample_df
 
-
-## Matching with the spatial data 406
+## Matching with the spatial data 
 ID              <- rownames(sample_df1)
 predicted_ports <- sps_df_tmp[rownames(sps_df_tmp@data)%in%ID,]
 predicted_ports <- SpatialPolygonsDataFrame(predicted_ports, 
                                             sample_df1, 
                                             match.ID = TRUE)
-predicted_ports <- predicted_ports[predicted_ports@data$prediction==1,]
-predicted_ports@data$prediction <- as.numeric(predicted_ports@data$prediction)
+predicted_ports@data$prediction_p <- prediction_p
+predicted_ports_l <- predicted_ports[predicted_ports@data$prediction==1,]
 
+writeOGR(predicted_ports_l,
+         ".",
+         "output/predicted_ports_l",
+         driver="ESRI Shapefile",
+         overwrite=TRUE)
 
+# tm_shape(coastline_study_area)+tm_lines(alpha=0.5)+
+#   tm_shape(predicted_ports)+tm_dots(size=0.5)
 
-tm_shape(coastline_study_area)+tm_lines(alpha=0.5)+
-  tm_shape(predicted_ports)+tm_dots()
